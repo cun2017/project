2dcb782ee240ecc7b1ee67417f3dcd619eb8be83
unknown <>
2019-05-21 14:23:35 +0800

    添加项目文件。

---
+++ NFine.Web/文明服务设计/files/文明服务评分（手机app端）/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,F,G,F,H,I,J,null,K,L,M,N,O,P,Q,L),R,_(),S,_(),T,_(U,[])),V,_(),W,_());}; 
+var b="url",c="文明服务评分（手机app端）.html",d="generationDate",e=new Date(1555487219224.48),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="Login",l="page",m="packageId",n="b43fb6ff042c403fa1526065ead5f415",o="type",p="Axure:Page",q="name",r="文明服务评分（手机app端）",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="center",y="fill",z="fillType",A="solid",B="color",C=0xFFF5F5F5,D="image",E="imageHorizontalAlignment",F="near",G="imageVerticalAlignment",H="imageRepeat",I="auto",J="favicon",K="sketchFactor",L="0",M="colorStyle",N="appliedColor",O="fontName",P="Applied Font",Q="borderWidth",R="adaptiveStyles",S="interactionMap",T="diagram",U="objects",V="masters",W="objectPaths";
+return _creator();
+})());
---
+++ WebApi/Controllers/UserModelsController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        public List<UserModel> allModeList = new List<UserModel>() {
+          new UserModel(){ Id=1,UserName="zhang", PassWord="123"},
+          new UserModel(){ Id=2,UserName="lishi", PassWord="123456"},
+          new UserModel(){ Id=3,UserName="wang", PassWord="1234567"}
+        };
+
+        //Get  api/User/ 
+        public IEnumerable<UserModel> GetAll()
+        {
+            return allModeList;
+        }
+
+        //Get api/User/1  
+        public IEnumerable<UserModel> GetOne(int id)
+        {
+            return allModeList.FindAll((m) => { return m.Id == id; });
+        }
+        
+
+        //POST api/User/  
+        public bool PostNew(UserModel user)
+        {
+            try
+            {
+                allModeList.Add(user);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //Delete api/User/ 
+        public int DeleteAll()
+        {
+            return allModeList.RemoveAll((mode) => { return true; });
+        }
+        //Delete api/User/1 
+        public IEnumerable<UserModel> DeleteOne(int id)
+        {
+             allModeList.RemoveAll((m) => { return m.Id == id; });
+            return allModeList;
+        }
+        //Put api/User  
+        public int PutOne(int id, UserModel user)
+        {
+            List<UserModel> upDataList = allModeList.FindAll((mode) => { return mode.Id == id; });
+            foreach (var mode in upDataList)
+            {
+                mode.PassWord = user.PassWord;
+                mode.UserName = user.UserName;
+            }
+            return upDataList.Count;
+        }
+    }
+}
